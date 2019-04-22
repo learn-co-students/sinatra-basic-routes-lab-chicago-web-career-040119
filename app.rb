cb3860afc13d6ebcd13f60ec94e17f81927a740b
Michael Wnek <beatawnek@Beatas-MacBook-Pro.local>
2019-04-22 14:08:59 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Michael!"
+end
+
+
+  get '/hometown' do
+    "My hometown is Hoffman Estates"
+  end
+
+
+  get '/favorite-song' do
+    "My favorite song is Swing"
+  end
 end
